052e5f9c476d11ebf168e2cf7f072b77688f8c0f
Zohar163 <Zohar163@163.com>
2024-09-03 17:35:03 +0800

    fix a bug:每次关闭ST再打开，并添加bookmark时，之前已添加的bookmark会全部丢失

--- BetterBookmarks.py
+++ BetterBookmarks.py
@@ -7,8 +7,9 @@ def plugin_loaded():
       os.makedirs(directory)
 
 def Log(message):
-   if Settings().get('verbose', False):
-      print('[BetterBookmarks] ' + message)
+   print('[BetterBookmarks] ' + message)
+   #if Settings().get('verbose', False):
+   #   print('[BetterBookmarks] ' + message)
 
 def Settings():
     return sublime.load_settings('BetterBookmarks.sublime-settings')
@@ -50,6 +51,7 @@ class BetterBookmarksCommand(sublime_plugin.TextCommand):
       h = hashlib.md5()
       h.update(self.view.file_name().encode())
       filename = str(h.hexdigest())
+      # print("缓存文件名：" + filename)
       return '{:s}/User/BetterBookmarks/{:s}.bb_cache'.format(sublime.packages_path(), filename)
 
    def _get_region_name(self, layer=None):
@@ -69,13 +71,13 @@ class BetterBookmarksCommand(sublime_plugin.TextCommand):
    def _add_marks(self, newMarks, layer=None):
       region = self._get_region_name(layer)
       marks = self.view.get_regions(region)
-      
       for mark in newMarks:
          if mark in marks:
             marks.remove(mark)
          else:
             marks.append(mark)
 
+      # print("marks 长度：" + str(len(marks)))
       self.view.add_regions(region, marks, '', '', 0)
 
       if layer == self.layer:
@@ -85,6 +87,10 @@ class BetterBookmarksCommand(sublime_plugin.TextCommand):
       layer = self.layer
       region = self._get_region_name(layer)
       marks = self.view.get_regions(region)
+
+      if len(marks) == 0:
+         Log("marks is empty")
+         return
 
       # 获取书签内容
       text_msg = []
@@ -97,12 +103,12 @@ class BetterBookmarksCommand(sublime_plugin.TextCommand):
 
       def on_done(index): # 选中索引从0开始
          if index != -1:
-            print(index)
+            Log(index)
             index_text = text_msg[index]
-            print("选中:" + index_text )
+            Log("选中:" + index_text )
             self.view.run_command('{}_bookmark'.format("prev"), {'name': 'better_bookmarks'})
          else:
-            print("取消选择")
+            Log("取消选择")
 
       # 弹出选项框，传入字符串数组和回调函数
       sublime.active_window().show_quick_panel(text_msg, on_done)
@@ -186,12 +192,14 @@ class BetterBookmarksCommand(sublime_plugin.TextCommand):
                for name, marks in data['bookmarks'].items():
                   self._add_marks([sublime.Region(mark[0], mark[1]) for mark in marks], name)
          except Exception as e:
+            Log("打开bookmark缓存文件异常：" + str(e))
             pass
          self._change_to_layer(Settings().get('default_layer'))
       elif subcommand == 'on_save':
          self._save_marks()
       elif subcommand == 'on_close':
          if Settings().get('cache_marks_on_close', False):
+            print("cache_marks_on_close")
             self._save_marks()
          if Settings().get('cleanup_empty_cache_on_close', False) and self._is_empty():
             Log('Removing BBFile for ' + self.filename)
@@ -199,6 +207,10 @@ class BetterBookmarksCommand(sublime_plugin.TextCommand):
                os.remove(self._get_cache_filename())
             except FileNotFoundError as e:
                pass
+
+# 定义一个全局变量，标记是否已经执行过on_load，没有执行过时，在文件活跃时重新调用一下on_load。
+# 否则每次关闭ST再打开，并添加bookmark时，之前已添加的bookmark会全部丢失
+did_run_on_load = 0
 
 class BetterBookmarksEventListener(sublime_plugin.EventListener):
    def __init__(self):
@@ -209,6 +221,12 @@ class BetterBookmarksEventListener(sublime_plugin.EventListener):
 
    def on_load_async(self, view):
       if Settings().get('uncache_marks_on_load'):
+         self._contact(view, 'on_load')
+
+   def on_activated_async(self, view):
+      global did_run_on_load
+      if did_run_on_load == 0:
+         did_run_on_load = 1
          self._contact(view, 'on_load')
 
    def on_pre_save(self, view):
